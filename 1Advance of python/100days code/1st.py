fe6d43ce29e684a6cd6d393bf3fad5f21e277e12
Ashraful-akash467 <ashrafulakash467@gmail.com>
2025-01-06 15:25:00 +0600

    Update

--- 1Advance of python/100days code/1st.py
+++
@@ -1,5 +0,0 @@
-print("Hello world")
-print (5)
-x=input("WHat is your name  :")
-
-print("HEy you are so good ",x)
--- 1Advance of python/100days code/3rd.py
+++
@@ -1,5 +0,0 @@
-for k in range(5,8):
-    print(k)
-
-for i in range(1,20,3):
-    print(i)
